5299912bb55a9aac5044e4ee52fa81a232aac7b6
exdysa <91800957+exdysa@users.noreply.github.com>
2025-01-26 14:45:45 -0500

    3.2.3 new node and string

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 @author:"˶𝞢⤬⫒ⵖsᐼ˶"
 @title: "Selector"
 @nickname: "Selector"
-@version: "3.2.2"
+@version: "3.2.3"
 @project: "https://github.com/exdysa/comfyui-selector",
 @description: "EXDYSA. Selector and Recourse. Presets & failsafes. Work flow."
 """
