82659004b5702707f4fe248f0d1afcba952617dc
exdysa <91800957+exdysa@users.noreply.github.com>
2025-01-26 14:44:26 -0500

    new adv selector node

--- selector.py
+++ selector.py
@@ -66,13 +66,13 @@ class Selectah:
         ("16:15_1024x960", 1024, 960),
         ("17:15_1088x960", 1088, 960),
         ("17:14_1088x896", 1088, 896),
-        ("4:3___1152x896", 1152, 896),
         ("18:13_1152x832", 1152, 832),
+        ("4:3___1152x896", 1152, 896),
         ("3:2___1216x832", 1216, 832),
-        ("72:32_1232x832", 1232, 832),
+        # ("72:32_1232x832", 1232, 832),
         ("5:3___1280x768", 1280, 768),
-        ("7:4___1344x768", 1344, 768),
         ("21:11_1344x704", 1344, 704),
+        ("7:4___1344x768", 1344, 768),
         ("2:1___1408x704", 1408, 704),
         ("23:11_1472x704", 1472, 704),
         ("21:9__1536x640", 1536, 640),
@@ -88,12 +88,13 @@ class Selectah:
         ("1:1___V_256x256", 256, 256),
         ("4:3___V_320x240", 320, 240),
         ("32:27_V_576x486", 576, 486),
-        ("4:3___V_832x624", 832, 624),
         ("22:15_V_704x480", 704, 480),
         ("9:5___V_720x400", 720, 400),
         ("3:2___V_720x480", 720, 480),
         ("5:4___V_720x576", 720, 576),
         ("3:2___V_768x512", 768, 512),
+        ("4:3___V_832x624", 832, 624),
+        ("53:30_V_848x480", 848, 480),
         ("4:3___V 960x704", 960, 704),
         ("1:1___V_960x960", 960, 960),
         ("20:11_V_1280x704", 1280, 704),
@@ -102,9 +103,9 @@ class Selectah:
         ("1:1____SD_512x512", 512, 512),
         ("4:3____SD_682x512", 682, 512),
         ("3:2____SD_768x512", 768, 512),
+        ("1:1____SD_768x768", 768, 768),
         ("16:9___SD_910x512", 910, 512),
         ("1:85:1_SD_952x512", 952, 512),
-        ("1:1___SD2_768x768", 768, 768),
         ("2:1____SD_1024x512", 1024, 512),
     ]
 
@@ -227,7 +228,7 @@ class Selectah:
         "REFINER_STEPS",
         "CFG",
         "REFINER_CFG",
-        "STRENGTH",
+        "DENOISE",
         "VARIATION_STR",
         "SCALE",
         "SAMPLER",
@@ -255,6 +256,177 @@ class Selectah:
 
         # If aspect ratio is not found, return None for width and height
         return (None, None, batch, steps, refiner_steps, cfg, refiner_cfg, str_denoise, scale, variation_str, sampler, scheduler)
+
+
+class SelectahAdv:
+    """Choose settings for advanced nodes in a single place"""
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        """User inputs"""
+        return {
+            "required": {},
+            "optional": {
+                "clip_skip": (
+                    "INT",
+                    {
+                        "default": -2,
+                        "min": -10000,
+                        "max": -1,
+                    },
+                ),
+                "str_1": (
+                    "FLOAT",
+                    {
+                        "default": 1.000,
+                        "min": 0.000,
+                        "max": 1000.000,
+                        "step": 0.001,
+                        "round": 0.001,
+                    },
+                ),
+                "str_2": (
+                    "FLOAT",
+                    {
+                        "default": 1.000,
+                        "min": 0.000,
+                        "max": 1000.000,
+                        "step": 0.001,
+                        "round": 0.001,
+                    },
+                ),
+                "str_3": (
+                    "FLOAT",
+                    {
+                        "default": 1.000,
+                        "min": 0.000,
+                        "max": 1000.000,
+                        "step": 0.001,
+                        "round": 0.001,
+                    },
+                ),
+                "str_4": (
+                    "FLOAT",
+                    {
+                        "default": 1.000,
+                        "min": 0.000,
+                        "max": 1000.000,
+                        "step": 0.001,
+                        "round": 0.001,
+                    },
+                ),
+                "shift_1": (
+                    "FLOAT",
+                    {
+                        "default": 1.150,
+                        "min": 0.000,
+                        "max": 1000.000,
+                        "step": 0.001,
+                        "round": 0.001,
+                    },
+                ),
+                "shift_2": (
+                    "FLOAT",
+                    {
+                        "default": 1.730,
+                        "min": 0.000,
+                        "max": 1000.000,
+                        "step": 0.001,
+                        "round": 0.001,
+                    },
+                ),
+                "shift_3": (
+                    "FLOAT",
+                    {
+                        "default": 3.000,
+                        "min": 0.000,
+                        "max": 1000.000,
+                        "step": 0.001,
+                        "round": 0.001,
+                    },
+                ),
+                "shift_4": (
+                    "FLOAT",
+                    {
+                        "default": 0.500,
+                        "min": 0.000,
+                        "max": 1000.000,
+                        "step": 0.001,
+                        "round": 0.001,
+                    },
+                ),
+                "alt_scale": (
+                    "FLOAT",
+                    {
+                        "default": 1.000,
+                        "min": 0.000,
+                        "max": 1000.000,
+                        "step": 0.001,
+                        "round": 0.001,
+                    },
+                ),
+            },
+        }
+
+    RETURN_TYPES = (
+        "INT",
+        "FLOAT",
+        "FLOAT",
+        "FLOAT",
+        "FLOAT",
+        "FLOAT",
+        "FLOAT",
+        "FLOAT",
+        "FLOAT",
+        "FLOAT",
+    )
+
+    RETURN_NAMES = (
+        "CLIP_SKIP",
+        "STR_1",
+        "STR_2",
+        "STR_3",
+        "STR_4",
+        "SHIFT_1",
+        "SHIFT_2",
+        "SHIFT_3",
+        "SHIFT_4",
+        "ALT_SCALE",
+    )
+    FUNCTION = "selectah_adv"
+    CATEGORY = SELECTOR_CATEGORY_PATH
+
+    def selectah_adv(
+        self,
+        clip_skip=None,
+        str_1=None,
+        str_2=None,
+        str_3=None,
+        str_4=None,
+        shift_1=None,
+        shift_2=None,
+        shift_3=None,
+        shift_4=None,
+        alt_scale=None,
+    ):
+        """
+        Choose advanced workflow settings from a central place\n
+        """
+        return (
+            clip_skip,
+            str_1,
+            str_2,
+            str_3,
+            str_4,
+            shift_1,
+            shift_2,
+            shift_3,
+            shift_4,
+            alt_scale,
+        )
 
 
 class RecourseCheckpoint:
@@ -340,7 +512,7 @@ class RecourseStrings:
     def INPUT_TYPES(s):
         return {
             "required": {
-                "string_1": (
+                "text_1": (
                     "STRING",
                     {
                         "multiline": True,
@@ -350,7 +522,7 @@ class RecourseStrings:
                 ),
             },
             "optional": {
-                "string_2": (
+                "text_2": (
                     "STRING",
                     {
                         "multiline": True,
@@ -358,7 +530,7 @@ class RecourseStrings:
                         "dynamicPrompts": True,
                     },
                 ),
-                "string_3": (
+                "text_3": (
                     "STRING",
                     {
                         "multiline": True,
@@ -375,15 +547,13 @@ class RecourseStrings:
         "STRING",
         "STRING",
         "STRING",
-        "STRING",
         "INT",
         "INT",
     )
     RETURN_NAMES = (
-        "LINKED_STRINGS",
-        "STRING_1",
-        "STRING_2",
-        "STRING_3",
+        "TEXT_1",
+        "TEXT_2",
+        "TEXT_3",
         "SEED",
         "NOISE_SEED",
     )
@@ -392,35 +562,27 @@ class RecourseStrings:
 
     CATEGORY = SELECTOR_CATEGORY_PATH
 
-    def recourse_string(
-        self,
-        seed,
-        noise_seed,
-        string_1,
-        string_2="",
-        string_3="",
-    ):
-        data = [string_1, string_2, string_3]
-        full_out = "".join(data)
+    def recourse_string(self, text_1, seed=0, noise_seed=0, text_2="", text_3=""):
+        """
+        Send text to different locations in a workflow\n
+        :return: Tuple of strings and seeds
+        """
+        # data = [string_1, string_2, string_3]
+        # full_out = "".join(data)
 
-        return (
-            full_out,
-            string_1,
-            string_2,
-            string_3,
-            seed,
-            noise_seed,
-        )
+        return (text_1, text_2, text_3, seed, noise_seed)
 
 
 NODE_CLASS_MAPPINGS = {
     "Selector": Selectah,
+    "Selector Advanced": SelectahAdv,
     "RecourseCkpt": RecourseCheckpoint,
     "RecourseStrings": RecourseStrings,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Selector": "Selector...",
+    "Selector Advanced": "Selector Advanced...",
     "RecourseCkpt": "RecourseCheck...",
     "RecourseStrings": "RecourseStrings...",
 }
